83c797ad3ae948cb78ca9cd7550fa9b67d7b1e09
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-08-05 01:14:05 +0700

    edit-category

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { create, list, remove } from "../controllers/categoory";
+import { create, list, remove, update } from "../controllers/categoory";
 
 const router = Router()
 
 router.post("/category", create)
 router.get("/category", list)
 router.delete("/category/:id", remove)
+router.delete("/category/:id", remove)
+router.put("/category/:id", update)
 
 export default router
