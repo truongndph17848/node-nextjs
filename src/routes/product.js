3b519cc33b188dfa4e9dc28e261415f2cf23479c
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-08-05 17:58:21 +0700

    api-search

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { create, list, read, remove, update } from "../controllers/product";
+import { create, list, read, remove, search, update } from "../controllers/product";
 
 const router = Router()
 
@@ -8,4 +8,6 @@ router.get("/product", list)
 router.get("/product/:id", read)
 router.delete("/product/:id", remove)
 router.put("/product/:id", update)
+router.post("/search", search)
+
 export default router
