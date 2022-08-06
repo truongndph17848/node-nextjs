b3eba3e5c4c819d258ea62c1f57f06a04e60e310
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-08-07 01:58:25 +0700

    fix: change routes category

--- src/routes/category.js
+++ src/routes/category.js
@@ -3,11 +3,11 @@ import { create, list, read, remove, update } from '../controllers/categoory';
 
 const router = Router()
 
-router.post("/category", create)
-router.get("/category", list)
-router.get("/category/:id", read);
-router.delete("/category/:id", remove)
-router.delete("/category/:id", remove)
-router.put("/category/:id", update)
+router.post("/categories", create)
+router.get("/categories", list)
+router.get("/categories/:id", read);
+router.delete("/categories/:id", remove)
+router.delete("/categories/:id", remove)
+router.put("/categories/:id", update)
 
 export default router
