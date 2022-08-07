7fc20df2b1b2d61fe25db737616ac350f91aa410
Le Duc Dung <dungldc6ts42020@gmail.com>
2022-08-07 14:21:18 +0700

    update: routes user

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import express from "express";
 import {login, register} from '../controllers/auth';
+import { listUser, removeUser, useById } from "../controllers/user";
 
-const router =express.Router();
+const router = Router();
 
 router.post('/register', register)
 router.post('/login', login)
+router.get("/users", listUser);
+router.get("/users/:id", useById);
+router.delete("/users/:id", removeUser);
 
 
 
